2757848941c31f841c25a5911b9e649086f2735f
mattyray <mnraynor90@gmail.com>
2025-02-18 12:33:39 -0500

    fixed crispy

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Middleware
 MIDDLEWARE = [
