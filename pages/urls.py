7fb436330b4c5f0fc85532d770161849377fa7e5
mattyray <mnraynor90@gmail.com>
2025-05-22 12:30:17 -0400

    adding new functionality - pages/urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HomePageView, contact_view, AboutPageView  # ✅ Correct import
+from .views import HomePageView, contact_view, AboutPageView, OtherWaysToHelpView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),  # ✅ Ensure `.as_view()` is used
     path('contact/', contact_view, name='contact'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('other-ways-to-help/', OtherWaysToHelpView.as_view(), name='other_ways_to_help'),
+
 
 
 
